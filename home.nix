07da2bc1bbe0b41004afaf474828235bbfc0f8ba
Origami404 <Origami404@foxmail.com>
2024-06-22 21:02:27 +0800

    [home] module-ize CLI tools

--- home.nix
+++ home.nix
@@ -6,39 +6,11 @@
 
   programs.home-manager.enable = true;
 
+  imports = [
+    (import ./modules/shell.nix)
+  ];
+
   home.packages = with pkgs;[
-    # Shell & Editor
-    fish
-
-    # Archives
-    atool # all-in-one tar
-
-    # Modern unix
-    ripgrep # better grep
-    jq # better cat on json
-    bat # better cat on everything
-    eza # better ls
-    fzf # A command-line fuzzy finder
-    du-dust # better du
-    duf # better df
-    fd # better find
-    btop # better top
-    procs # better ps
-    zoxide # better cd
-    scc # better cloc
-    btop # better top
-
-    # Networking
-    mtr # better traceroute
-    iperf3
-    q # better dig/nslookup
-    socat # better netcat
-    nmap
-
-    # Misc
-    grc
-    neofetch
-
     # system call monitoring
     strace # system call monitoring
     ltrace # library call monitoring
@@ -62,45 +34,6 @@
   programs.nix-index = {
     enable = true;
     enableFishIntegration = true;
-  };
-
-  programs.fzf.enableFishIntegration = false; # use fish.fzf keybinding
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      set fish_greeting # Disable greeting
-    '';
-    plugins = with pkgs.fishPlugins; [
-      { name = "z"; src = z.src; }
-      { name = "grc"; src = grc.src; }
-      { name = "fzf-fish"; src = fzf-fish.src; }
-      { name = "autopair"; src = autopair.src; }
-      { name = "colored-man-pages"; src = colored-man-pages.src; }
-    ];
-    shellInit = ''
-      fzf_configure_bindings --directory=\cp --git_log=\cl --history=\cr \
-        --git_status= --processes= --variables=
-    '';
-    shellAbbrs = {
-      "..." = "../..";
-      "...." = "../../..";
-    };
-    shellAliases = {
-      "ls" = "eza";
-      "ll" = "eza -l";
-      "la" = "eza -a";
-      "grep" = "rg";
-      "top" = "btop";
-      "dig" = "q";
-      "find" = "fd";
-      "du" = "dust";
-      "df" = "duf";
-      "cloc" = "scc";
-      "cat" = "bat";
-      "x" = "atool -x";
-      "flake-update" = "nixos-rebuild switch --use-remote-sudo --flake ${config.home.homeDirectory}/flakes#um790-nix";
-      "cl" = "clear";
-    };
   };
 
   programs.vscode = {
---
+++ modules/shell.nix
@@ -0,0 +1,59 @@
+{ pkgs, lib, config, ... }:
+{
+  home.packages = with pkgs; [
+    atool # all-in-one tar
+    ripgrep # better grep
+    jq # better cat on json
+    bat # better cat on everything
+    eza # better ls
+    fzf # A command-line fuzzy finder
+    du-dust # better du
+    duf # better df
+    fd # better find
+    btop # better top
+    procs # better ps
+    scc # better cloc
+    btop # better top
+    grc # log file colorize
+    neofetch # tool for showing off 
+  ];
+
+  programs.fzf.enableFishIntegration = false; # use fish.fzf keybinding
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting # Disable greeting
+    '';
+    plugins = with pkgs.fishPlugins; [
+      { name = "z"; src = z.src; }
+      { name = "grc"; src = grc.src; }
+      { name = "fzf-fish"; src = fzf-fish.src; }
+      { name = "autopair"; src = autopair.src; }
+      { name = "colored-man-pages"; src = colored-man-pages.src; }
+    ];
+    shellInit = ''
+      fzf_configure_bindings --directory=\cp --git_log=\cl --history=\cr \
+        --git_status= --processes= --variables=
+    '';
+    shellAbbrs = {
+      "..." = "../..";
+      "...." = "../../..";
+    };
+    shellAliases = {
+      "ls" = "eza";
+      "ll" = "eza -l";
+      "la" = "eza -a";
+      "grep" = "rg";
+      "top" = "btop";
+      "dig" = "q";
+      "find" = "fd";
+      "du" = "dust";
+      "df" = "duf";
+      "cloc" = "scc";
+      "cat" = "bat";
+      "x" = "atool -x";
+      "flake-update" = "nixos-rebuild switch --use-remote-sudo --flake ${config.home.homeDirectory}/flakes#um790-nix";
+      "cl" = "clear";
+    };
+  };
+}
