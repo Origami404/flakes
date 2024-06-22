a12a393e8e360703811643499f3eff34288ac2ac
Origami404 <Origami404@foxmail.com>
2024-06-22 14:46:12 +0800

    add nixd

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,7 @@
     # it provides the command `nom` works just like `nix`
     # with more details log output
     nix-output-monitor
+    nixd
 
     # system call monitoring
     strace   # system call monitoring
