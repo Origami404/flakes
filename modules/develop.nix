4fe5d0a1596be4b15c7998024f6e3660e9324799
Origami404 <Origami404@foxmail.com>
2024-06-24 00:16:56 +0800

    [home][develop] add git

--- modules/develop.nix
+++ modules/develop.nix
@@ -14,5 +14,22 @@
     direnv hook fish | source
   '';
 
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    userName = "Origami404";
+    userEmail = "Origami404@foxmail.com";
+    extraConfig = {
+      user.signingKey = "5C7FCAFBC961E2E312B47FEC76447D4671CB3CC0";
+      core.autocrlf = "input";
+      push.autoSetupRemote = true;
+      pull.rebase = true;
+      init.defaultBranch = "main";
+    };
+    aliases = {
+      "l" = "log --date-order --graph --pretty=\"format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset\"";
+    };
+  };
+
   programs.vscode.enable = true;
 }
